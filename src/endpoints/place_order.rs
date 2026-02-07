8939cf204996b97367f943072c7736433ae9048a
Rishi <git@fushinori.dev>
2026-02-07 17:34:54 +0530

    refactor(endpoint): borrow PlaceOrderRequest instead of taking ownership

--- src/endpoints/place_order.rs
+++ src/endpoints/place_order.rs
@@ -4,8 +4,8 @@ use crate::{Fyers, FyersError, Order, PlaceOrderRequest};
 
 impl Fyers {
     /// Place a single order
-    pub async fn place_order(&self, order: PlaceOrderRequest) -> Result<Order, FyersError> {
-        let response = self.post("orders/sync", &order).await?;
+    pub async fn place_order(&self, order: &PlaceOrderRequest) -> Result<Order, FyersError> {
+        let response = self.post("orders/sync", order).await?;
         Ok(Order::deserialize(response)?)
     }
 }
