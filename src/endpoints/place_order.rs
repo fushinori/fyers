a4954c34547243a74f8f3ef167e4ad9f10936e72
Rishi <git@fushinori.dev>
2026-02-18 18:35:26 +0530

    docs(orders): expand documentation for `place_order` endpoint

--- src/endpoints/place_order.rs
+++ src/endpoints/place_order.rs
@@ -5,7 +5,28 @@ use crate::urls;
 use crate::{Fyers, FyersError, Order};
 
 impl Fyers {
-    /// Place a single order
+    /// Place a single order.
+    ///
+    /// Returns the placed [`Order`] on success.
+    ///
+    /// Use [`OrderRequest::builder`] to construct the order.
+    ///
+    /// # Example
+    /// ```no_run
+    /// let order = OrderRequest::builder(
+    ///    "NSE:JIOFIN-EQ",
+    ///    1,
+    ///    OrderType::Market,
+    ///    Side::Buy,
+    ///    ProductType::Intraday,
+    ///    Validity::Day,
+    /// )
+    /// .offline_order(true) // Optional args here
+    /// .build(); // build the request
+    ///
+    /// let order = fyers.place_order(&order).await?;
+    /// println!("{order:?}");
+    /// ```
     pub async fn place_order(&self, order: &OrderRequest) -> Result<Order, FyersError> {
         let url = format!("{}/orders/sync", urls::API_V3);
         let response = self.post(&url, order).await?;
